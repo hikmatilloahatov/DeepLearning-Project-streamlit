700f885f88452a906894faa3c8a7b297b8671667
Hikmatillo Ahatov <90811172+hikmatilloahatov@users.noreply.github.com>
2022-04-27 17:06:29 +0500

    Streamlit project

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from fastai.vision.all import *
+import plotly.express as px
+
+#title
+st.title('DeepLearning Project')
+st.subheader('Classification model for transports')
+
+#file_uploader
+file = st.file_uploader('Upload image', type=['svg', 'png', 'jpg', 'jpeg'])
+
+#display and predict it
+if file and file.type != 'image/svg+xml':
+    
+    st.image(file)
+    image = PILImage.create(file)
+    model = load_learner('transport_model.pkl')
+    
+    pred, pred_id, probs = model.predict(image)
+
+    st.success(f"This is a {pred}")
+    st.info(f"Probability: {probs[pred_id]*100:.1f}%")
+    #plotly chart
+    fig = px.bar(x=probs*100, y=model.dls.vocab)
+    st.plotly_chart(fig)
+else:
+    st.info("Please, don't upload image of svg format!")
+
